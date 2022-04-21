61e91c5d5912fbb700a4d447f6c6d2af36cfa082
Simre1 <simre4775@gmail.com>
2022-04-21 02:08:27 +0200

    README updates

---
+++ reactive-markup-gtk/example/Hello.hs
@@ -0,0 +1,20 @@
+import ReactiveMarkup.Target.Gtk
+import ReactiveMarkup
+import Data.Void
+
+
+main :: IO ()
+main = do
+  runGtk app
+
+renderGUI :: Markup Gtk Root Void
+renderGUI = bold "Hello Reactive Markup"
+
+app :: App Gtk EmptyF Void
+app =
+  App
+    { appRender = \_ -> renderGUI,
+      appHandleEvent = absurd,
+      appInitialState = EmptyF,
+      appName = "Hello Reactive Markup"
+    }
---
+++ reactive-markup/src/ReactiveMarkup.hs
@@ -0,0 +1,8 @@
+module ReactiveMarkup 
+  (module Markup, module Context, module Widget, module App, module RHKT) where
+
+import Data.RHKT as RHKT
+import ReactiveMarkup.App as App
+import ReactiveMarkup.Widget as Widget
+import ReactiveMarkup.Context as Context
+import ReactiveMarkup.Markup as Markup
